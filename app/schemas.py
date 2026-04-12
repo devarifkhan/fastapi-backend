428f66fd4affaa5c10cde6caf0b7e5f5de8c367d
devarifkhan <arifcse209@gmail.com>
2026-04-12 11:59:14 +0600

    Refactor Shipment model to use random_destination for default destination value

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,11 @@
 from pydantic import BaseModel,Field
+from random import randint
+
+
+def random_destination() -> int:
+    return randint(11000, 11999)
 
 class Shipment(BaseModel):
     content: str = Field(max_length=30)
     weight: float = Field(le=25,ge=1)
-    destination: int | None = Field(default=None, ge=1)
+    destination: int | None = Field(default_factory=random_destination)
