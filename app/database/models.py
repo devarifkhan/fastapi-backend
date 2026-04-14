ea081970953b39ea9d3bc4814521a7a2b8091253
devarifkhan <arifcse209@gmail.com>
2026-04-14 13:43:10 +0600

    Add Seller model and mypy configuration for type checking

--- app/database/models.py
+++ app/database/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from enum import Enum
 
+from pydantic import EmailStr
 from sqlmodel import Field, SQLModel
 
 
@@ -20,3 +21,11 @@ class Shipment(SQLModel, table = True):
     destination: int
     status: ShipmentStatus
     estimated_delivery: datetime
+
+class Seller(SQLModel, table = True):
+    __tablename__ = "seller"
+
+    id: int = Field(default=None, primary_key=True)
+    name: str
+    email: EmailStr
+    password: str
